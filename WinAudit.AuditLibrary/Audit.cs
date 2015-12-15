390d4d49902d20d52eca37f4d8b82ba53a35ede0
Allister Beharry <allister.beharry@gmail.com>
2015-12-15 09:58:41 -0400

    Added unit tests project, GetMSIPackages code.

--- WinAudit.AuditLibrary/Audit.cs
+++ WinAudit.AuditLibrary/Audit.cs
@@ -30,6 +30,8 @@ using System.Net.Http.Headers;
 using System.Xml;
 using System.Xml.Linq;
 using System.Linq;
+using System.Security;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -46,28 +48,42 @@ namespace WinAudit.AuditLibrary
             RegistryKey k = null;
             try
             {
+                RegistryPermission perm = new RegistryPermission(RegistryPermissionAccess.Read, @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+                perm.Demand();
                 k = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
                 IEnumerable <OSSIndexQueryObject> packages_query =
                     from sn in k.GetSubKeyNames()
-                    select new OSSIndexQueryObject("nuget", (string) k.OpenSubKey(sn).GetValue("DisplayName"),
-                        (string) k.OpenSubKey(sn).GetValue("DisplayName"), 
+                    select new OSSIndexQueryObject("msi", (string) k.OpenSubKey(sn).GetValue("DisplayName"),
+                        (string) k.OpenSubKey(sn).GetValue("DisplayVersion"), 
                         (string) k.OpenSubKey(sn).GetValue("Publisher"));
-                List<OSSIndexQueryObject> packages = packages_query.ToList<OSSIndexQueryObject>();
+                List<OSSIndexQueryObject> packages = packages_query
+                    .Where(p => !string.IsNullOrEmpty(p.ApplicationName))
+                    .ToList<OSSIndexQueryObject>();
+
+                perm = new RegistryPermission(RegistryPermissionAccess.Read, @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall");
+                perm.Demand();
                 k = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall");
                 packages_query = 
                     from sn in k.GetSubKeyNames()
-                    select new OSSIndexQueryObject("nuget", (string) k.OpenSubKey(sn).GetValue("DisplayName"),
-                        (string) k.OpenSubKey(sn).GetValue("DisplayName"), (string) k.OpenSubKey(sn).GetValue("Publisher"));
-                packages.AddRange(packages_query);
+                    select new OSSIndexQueryObject("msi", (string) k.OpenSubKey(sn).GetValue("DisplayName"),
+                        (string) k.OpenSubKey(sn).GetValue("DisplayVersion"), (string) k.OpenSubKey(sn).GetValue("Publisher"));
+                packages.AddRange(packages_query.Where(p => !string.IsNullOrEmpty(p.ApplicationName)));
+
+                perm = new RegistryPermission(RegistryPermissionAccess.Read, @"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+                perm.Demand();
                 k = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
                 packages_query =
                     from sn in k.GetSubKeyNames()
-                    select new OSSIndexQueryObject("nuget", (string)k.OpenSubKey(sn).GetValue("DisplayName"),
-                        (string)k.OpenSubKey(sn).GetValue("DisplayName"), (string)k.OpenSubKey(sn).GetValue("Publisher"));
-                packages.AddRange(packages_query);
+                    select new OSSIndexQueryObject("msi", (string)k.OpenSubKey(sn).GetValue("DisplayName"),
+                        (string)k.OpenSubKey(sn).GetValue("DisplayVersion"), (string)k.OpenSubKey(sn).GetValue("Publisher"));
+                packages.AddRange(packages_query.Where(p => !string.IsNullOrEmpty(p.ApplicationName)));
                 return packages;
             }
 
+            catch (SecurityException se)
+            {
+                throw new Exception("Security exception thrown reading registry key: " + (k == null ? "" : k.Name + "\n" + se.Message), se);
+            }
             catch (Exception e)
             {
                 throw new Exception("Exception thrown reading registry key: " + (k == null ? "" : k.Name), e);
---
+++ WinAudit.Tests/AuditLibrary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+using WinAudit.AuditLibrary;
+namespace WinAudit.Tests
+{
+    public class AuditLibrary
+    {
+        [Fact]
+        public void CanGetMSIPackages()
+        {
+            Audit a = new Audit();
+            IEnumerable<OSSIndexQueryObject> packages = a.GetMSIPackages();
+            Assert.NotEmpty(packages);
+        }
+    }
+}
