b68f65805e66c1a6c7bef94c46df27ff3e56b703
Allister Beharry <allister.beharry@gmail.com>
2015-12-26 21:13:45 -0400

    Added CommandLine project and disk cache implementation.

---
+++ WinAudit.CommandLine/Options.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommandLine;
+using CommandLine.Text;
+
+namespace WinAudit.CommandLine
+{
+    class Options
+    {
+        [Option('o', "oneget", Required = false, HelpText = "Audit OneGet packages.")]
+        public bool AuditOneGet { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ WinAudit.CommandLine/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using CL = CommandLine; //Avoid type name conflict with external CommandLine library
+using CommandLine.Text;
+using CSharpTest.Net.Collections;
+using CSharpTest.Net.Serialization;
+using Newtonsoft.Json;
+
+using WinAudit.AuditLibrary;
+
+namespace WinAudit.CommandLine
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var options = new Options();
+
+            if (!CL.Parser.Default.ParseArguments(args, options))
+            {
+                return 1;
+            }
+
+            BPlusTree<string, OSSIndexQueryResultObject>.OptionsV2 cache_file_options = new BPlusTree<string, OSSIndexQueryResultObject>.OptionsV2(PrimitiveSerializer.String,
+                new BsonSerializer<OSSIndexQueryResultObject>());
+            cache_file_options.CalcBTreeOrder(4, 128);
+            cache_file_options.CreateFile = CreatePolicy.IfNeeded;
+            cache_file_options.FileName = AppDomain.CurrentDomain.BaseDirectory + "winaudit-net.cache"; //Assembly.GetExecutingAssembly().Location
+            cache_file_options.StoragePerformance = StoragePerformance.CommitToDisk;
+            BPlusTree<string, OSSIndexQueryResultObject> cache = new BPlusTree<string, OSSIndexQueryResultObject>(cache_file_options);
+            if (options.AuditOneGet)
+            {
+                Console.WriteLine("Scanning OneGet packages...");
+                Audit audit = new Audit("1.1");
+                IEnumerable<OSSIndexQueryObject> packages = audit.GetOneGetPackages();
+                Console.WriteLine("Found {0} OneGet packages.", packages.Count());
+            }
+
+
+            return 0;
+        }
+    }
+}
