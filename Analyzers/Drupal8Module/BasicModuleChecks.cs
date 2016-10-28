4d70ee83a65b5c8d1c73328fe85034289f391efc
Allister Beharry <allister.beharry@gmail.com>
2016-10-28 17:10:31 -0400

    Copy Rules and Analyzers to root folder. Delete Tests and other folders in Dockerfile build.

---
+++ Analyzers/Drupal8Module/BasicModuleChecks.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using Devsense.PHP.Syntax;
+using Devsense.PHP.Syntax.Ast;
+
+using DevAudit.AuditLibrary;
+
+namespace DevAudit.AuditLibrary.Analyzers.CSharp.Analyzers.Drupal8Module
+{
+    public class BasicModuleChecksAnalyzer : PHPAnalyzer
+    {
+        #region Constructors
+        public BasicModuleChecksAnalyzer(ScriptEnvironment script_env, object workspace, object project, object compilation) : base(script_env, "BasicModuleChecks", workspace, project, compilation)
+        {
+            this.Summary = "Basic sanity checks for a Drupal 8 module based on " + @"https://www.drupal.org/docs/8/creating-custom-modules";
+            this.Description = "";
+        }
+        #endregion
+
+        #region Overriden methods
+        public override Task<AnalyzerResult> Analyze()
+        {
+            AnalyzerResult ar = new AnalyzerResult() { Analyzer = this };
+            if (this.JsonFiles == null || !this.JsonFiles.Any(jf => jf.Name == "composer.json"))
+            {
+                this.ScriptEnvironment.Info("The module directory does not contain a composer.json file");
+                ar.IsVulnerable = true;
+            }
+            //return Task.FromResult(ar);
+            
+            CheckClassesDeclaredTreeVisitor ccdv = new CheckClassesDeclaredTreeVisitor(new List<string>() { "ControllerBase" });
+            foreach (var su in this.PHPSourceUnits)
+            {
+                su.Value.Ast.VisitMe(ccdv);
+            }
+
+                if (ccdv.ClassesDeclared.All(kv => kv.Value))
+                {
+                    this.ScriptEnvironment.Info("Found ControllerBase class");
+                    
+                }
+            
+            return Task.FromResult(ar);
+        }
+        
+        #endregion
+
+        #region Private types
+       
+        #endregion
+    }
+}
---
+++ Analyzers/Roslyn/SA1300ElementMustBeginWithUpperCaseLetterAnalyzer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using DevAudit.AuditLibrary;
+
+namespace DevAudit.AuditLibrary.Analyzers
+{
+    public class SA1300ElementMustBeginWithUpperCaseLetterAnalyzer : StyleCopAnalyzer
+    {
+        #region Constructors
+        public SA1300ElementMustBeginWithUpperCaseLetterAnalyzer(ScriptEnvironment script_env, object workspace, object project, object compilation) : base(script_env, "SA1300ElementMustBeginWithUpperCaseLetter", workspace, project, compilation)
+        {
+            this.Summary = "The name of a C# element does not begin with an upper-case letter.";
+            this.Description = "A violation of this rule occurs when the names of certain types of elements do not begin with an upper-case letter. The following types of elements should use an upper-case letter as the first letter of the element name: namespaces, classes, enums, structs, delegates, events, methods, and properties. In addition, any field which is public, internal, or marked with the const attribute should begin with an upper-case letter. Non-private readonly fields must also be named using an upper-case letter. If the field or variable name is intended to match the name of an item associated with Win32 or COM, and thus needs to begin with a lower-case letter, place the field or variable within a special NativeMethods class. A <c>NativeMethods</c> class is any class which contains a name ending in NativeMethods, and is intended as a placeholder for Win32 or COM wrappers. StyleCop will ignore this violation if the item is placed within a NativeMethods class.";
+        }
+        #endregion
+
+        #region Public overriden methods
+        public override async Task<AnalyzerResult> Analyze()
+        {
+            foreach (SyntaxTree tree in this.Compilation.SyntaxTrees)
+            {
+                CompilationUnitSyntax root = (CompilationUnitSyntax) await tree.GetRootAsync();
+                List<TypeDeclarationSyntax> declarations = root.DescendantNodesAndSelf().Where(
+                    x => x.IsKind(SyntaxKind.InterfaceDeclaration) ||
+                    x.IsKind(SyntaxKind.ClassDeclaration) ||
+                    x.IsKind(SyntaxKind.StructDeclaration)
+                    ).Select(d => d as TypeDeclarationSyntax).ToList();
+                
+                if (declarations.Count == 0)
+                {
+                    continue;
+                }
+                this.ScriptEnvironment.Info("Got {0} type declarations in compilation unit {1}: {2}", declarations.Count, tree.FilePath, declarations.Select(d => d.Identifier.ValueText).Where(i => !string.IsNullOrEmpty(i)).Aggregate((s1, s2) => s1 + " " + s2));
+               
+                /*
+                foreach (TypeDeclarationSyntax d in declarations)
+                {
+                    string identifier = d.Identifier.ValueText;
+                    this.ScriptEnvironment.Debug(c.Identifier.ValueText);
+                }*/
+            }
+            return this.AnalyzerResult;
+        }
+        #endregion
+    }
+}
