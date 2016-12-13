03bf67f9d5ee774ffdb0e2bd824ce3f5911104e5
Allister Beharry <allister.beharry@gmail.com>
2016-12-13 19:42:10 -0400

    Don't clear existing package sources in NuGet.Config. Only try to compile if Nuget restore completes successfully.

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-nuget restore DevAudit.Mono.sln
-xbuild DevAudit.Mono.sln /p:Configuration=RuntimeDebug
+nuget restore DevAudit.Mono.sln && xbuild DevAudit.Mono.sln /p:Configuration=RuntimeDebug
