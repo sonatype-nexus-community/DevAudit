18f9c759943b374086204ca9af0bd2f878712027
Allister Beharry <allister.beharry@gmail.com>
2017-01-26 11:04:23 -0400

    Removed root dir analyzers from SCC, check for exception in WeakCrypto analyzer.

--- DevAudit.AuditLibrary.Analyzers.CSharp/Analyzers/NetFx/WeakCrypto.cs
+++ DevAudit.AuditLibrary.Analyzers.CSharp/Analyzers/NetFx/WeakCrypto.cs
@@ -26,50 +26,85 @@ namespace DevAudit.AuditLibrary.Analyzers
         #region Overriden methods
         public override Task<ByteCodeAnalyzerResult> Analyze()
         {
-            IEnumerable<MethodDefinition> methods = 
-            from type in this.Module.Types.Cast<TypeDefinition>()
-            from method in type.Methods.Cast<MethodDefinition>()
-            select method;
-
-            IEnumerable<TypeReference> type_references = this.Module.GetTypeReferences().Where(tr => tr.Name == "SHA1CryptoServiceProvider");
-
-            foreach (TypeReference tr in type_references)
+            try
             {
-                this.ScriptEnvironment.Info("Type reference to SHA1CryptoServiceProvider found in module {0}", this.Module.Name);
-                
-            }
-            
-            foreach (MethodDefinition md in methods)
-            {
-                this.ScriptEnvironment.Debug("Method {0} returns {1}.", md.Name, md.MethodReturnType.ReturnType.Name);
-                foreach (Instruction i in md.Body.Instructions.Where(i => i.OpCode == OpCodes.Call || i.OpCode == OpCodes.Callvirt))
+                IEnumerable<MethodDefinition> methods =
+                    from type in this.Module.Types.Cast<TypeDefinition>()
+                    from method in type.Methods.Cast<MethodDefinition>()
+                    select method;
+
+                IEnumerable<TypeReference> type_references = this.Module.GetTypeReferences().Where(tr => tr.Name == "SHA1CryptoServiceProvider" || tr.Name == "MD5CryptoServiceProvider");
+
+                if (type_references != null && type_references.Any())
                 {
-                    MethodReference mr = (MethodReference)i.Operand;
-                    if (mr.FullName.Contains("System.Security.Cryptography.HashAlgorithm::ComputeHash"))
+                    foreach (TypeReference tr in type_references)
                     {
-                        this.ScriptEnvironment.Info("Detected a call to System.Security.Cryptography.HashAlgorithm::ComputeHash in method {0}", md.FullName);
-                        this.AnalyzerResult = new ByteCodeAnalyzerResult()
-                        {
-                            Analyzer = this,
-                            Executed = true,
-                            Succeded = true,
-                            IsVulnerable = true,
-                            ModuleName = this.Module.Name,
-                            LocationDescription = md.FullName
-                        };
-                        return Task.FromResult(this.AnalyzerResult);
+                        this.ScriptEnvironment.Info("Type reference to SHA1CryptoServiceProvider found in module {0}", this.Module.Name);
+
                     }
+
+                    foreach (MethodDefinition md in methods)
+                    {
+                        this.ScriptEnvironment.Debug("Method {0} returns {1}.", md.Name, md.MethodReturnType.ReturnType.Name);
+                        foreach (Instruction i in md.Body.Instructions.Where(i => i.OpCode == OpCodes.Call || i.OpCode == OpCodes.Callvirt && i.Operand != null))
+                        {
+                            MethodReference mr = (MethodReference)i.Operand;
+                            if (mr.FullName.Contains("System.Security.Cryptography.HashAlgorithm::ComputeHash"))
+                            {
+                                this.ScriptEnvironment.Info("Detected a call to System.Security.Cryptography.HashAlgorithm::ComputeHash in method {0}", md.FullName);
+                                this.AnalyzerResult = new ByteCodeAnalyzerResult()
+                                {
+                                    Analyzer = this,
+                                    Executed = true,
+                                    Succeded = true,
+                                    IsVulnerable = true,
+                                    ModuleName = this.Module.Name,
+                                    LocationDescription = md.FullName
+                                };
+                                return Task.FromResult(this.AnalyzerResult);
+                            }
+                        }
+                    }
+                    return Task.FromResult(new ByteCodeAnalyzerResult()
+                    {
+                        Analyzer = this,
+                        ModuleName = this.Module.Name,
+                        Executed = true,
+                        Succeded = true,
+                        IsVulnerable = false,
+
+                    });
+                }
+                else
+                {
+                    return Task.FromResult(new ByteCodeAnalyzerResult()
+                    {
+                        Analyzer = this,
+                        ModuleName = this.Module.Name,
+                        Executed = true,
+                        Succeded = true,
+                        IsVulnerable = false,
+
+                    });
                 }
             }
-            return Task.FromResult(new ByteCodeAnalyzerResult()
+            catch (Exception e)
             {
-                Analyzer = this,
-                ModuleName = this.Module.Name,
-                Executed = true,
-                Succeded = true,
-                IsVulnerable = false,
+                ScriptEnvironment.Error(e);
+                List<Exception> exceptions = new List<Exception> { e };
+                if (e.InnerException != null) exceptions.Add(e.InnerException);
+                return Task.FromResult(new ByteCodeAnalyzerResult()
+                {
+                    Analyzer = this,
+                    ModuleName = this.Module.Name,
+                    Executed = true,
+                    Succeded = false,
+                    Exceptions = exceptions,
+                    IsVulnerable = false,
 
-            });
+
+                });
+            }
         }
         #endregion
     }
