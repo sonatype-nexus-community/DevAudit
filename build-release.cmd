38c1f49a252c50920b282936c173281c38f0895b
Allister Beharry <allister.beharry@gmail.com>
2017-03-08 19:53:21 -0400

    Check for dirs after build.

--- build-release.cmd
+++ build-release.cmd
@@ -8,9 +8,6 @@ if "%3" == "" goto ArgError
 if "%4" == "" goto ArgError
 if "%5" == "" goto ArgError
 if not exist "DevAudit.sln" goto SlnError
-if not exist ".\Analyzers" goto AnalyzersDirError
-if not exist ".\Rules" goto RulesDirError
-if not exist ".\Examples" goto ExamplesDirError
 if not exist "%5" goto OutputDirError
 set MAJOR=%1
 set MINOR=%2
@@ -19,6 +16,9 @@ set BUILD=%4
 set OUTPUT=%5
 set RELEASE_DIR=%OUTPUT%\%MAJOR%.%MINOR%.%PATCH%.%BUILD%\DevAudit
 msbuild DevAudit.sln /p:Major=%MAJOR%;Minor=%MINOR%;Revision=%PATCH%;Build=%BUILD%;Configuration=RuntimeDebug;OutputPath=%RELEASE_DIR%
+if not exist ".\Analyzers" goto AnalyzersDirError
+if not exist ".\Rules" goto RulesDirError
+if not exist ".\Examples" goto ExamplesDirError
 mkdir %RELEASE_DIR%\Analyzers & xcopy /E /Y .\Analyzers %RELEASE_DIR%\Analyzers
 mkdir %RELEASE_DIR%\Rules & xcopy /E /Y .\Rules %RELEASE_DIR%\Rules
 mkdir %RELEASE_DIR%\Examples & xcopy /E /Y .\Examples %RELEASE_DIR%\Examples
@@ -43,15 +43,15 @@ echo The output directory %5% could not be found.
 goto Error
 
 :AnalyzersDirError
-echo The .\Analyzers directory could not be found.
+echo The .\Analyzers directory could not be found. An error may have occurred during build.
 goto Error
 
 :RulesDirError
-echo The .\Rules directory could not be found.
+echo The .\Rules directory could not be found. An error may have occurred during build.
 goto Error
 
 :ExamplesDirError
-echo The .\Examples directory could not be found.
+echo The .\Examples directory could not be found. An error may have occurred during build.
 goto Error
 
 :Error
