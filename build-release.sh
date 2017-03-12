7a3f2a2bd7247d7dccacc3eec47467d138182bd7
Allister Beharry <allister.beharry@gmail.com>
2017-03-11 20:28:19 -0400

    Add Linux build scripts and targets.

--- build-release.sh
+++ build-release.sh
@@ -13,31 +13,35 @@ then
 else
 	SPECIAL=""
 fi
-TAG=v$MAJOR.$MINOR.$PATCH
-BUILD_DIR=DevAudit.$TAG.Build
+TAG=v$MAJOR.$MINOR.x
+BUILD_TAG=$MAJOR.$MINOR.$PATCH.$BUILD
+RELEASE_TAG=$MAJOR.$MINOR.$PATCH.$BUILD$SPECIAL
+BUILD_DIR=DevAudit.$TAG.Build_$BUILD
 if [ -d "$BUILD_DIR" ]; then
 	echo Deleting $BUILD_DIR.
 	rm -rf "$BUILD_DIR"
 fi
 git clone --branch "$TAG" https://github.com/OSSIndex/DevAudit $BUILD_DIR
 if [[ $? -ne 0 ]]; then
-	echo An error occurred during checkout.
+	echo An error occurred during checkout of branch $TAG.
 	exit 1
 fi
 if [ ! -f $BUILD_DIR/DevAudit.Mono.sln ]; then
 	echo Could not find the solution file $BUILD_DIR/DevAudit.Mono.sln. An error may have occurred during git checkout.	
 	exit 1
 fi
-RELEASE_DIR=$MAJOR.$MINOR.$PATCH.$BUILD$SPECIAL
+RELEASE_DIR=$RELEASE_TAG
 if [ -d "$RELEASE_DIR" ]; then
 	echo Deleting $RELEASE_DIR.
 	rm -rf "$RELEASE_DIR"
 fi
 mkdir $RELEASE_DIR
 mkdir $RELEASE_DIR/DevAudit
-nuget restore $BUILD_DIR/DevAudit.Mono.sln && xbuild $BUILD_DIR/DevAudit.Mono.sln /p:Configuration=RuntimeDebug /p:VersionAssembly=$MAJOR.$MINOR.$PATCH.$BUILD
+nuget restore $BUILD_DIR/DevAudit.Mono.sln && xbuild $BUILD_DIR/DevAudit.Mono.sln /verbosity:diagnostic /p:Configuration=RuntimeDebug /p:VersionAssembly=$BUILD_TAG
 if [[ $? -ne 0 ]]; then
-	echo An error occurred during build.
+	echo An error occurred during build in $BUILD_DIR.
+    rm -rf "$BUILD_DIR"
+    rm -rf "$RELEASE_DIR"
 	exit 1
 fi
 cp $BUILD_DIR/DevAudit.AuditLibrary/bin/Debug/Gendarme.Rules.* ./DevAudit.CommandLine/bin/Debug/
@@ -47,3 +51,4 @@ chmod +x $RELEASE_DIR/DevAudit/devaudit
 cp -R $BUILD_DIR/DevAudit.CommandLine/bin/Debug/* $RELEASE_DIR/DevAudit 
 tar -cvzf DevAudit-$RELEASE_DIR.tgz $RELEASE_DIR/DevAudit
 rm -rf "$BUILD_DIR"
+echo Release $RELEASE_TAG created in directory $RELEASE_DIR and archive DevAudit-$RELEASE_DIR.tgz.
