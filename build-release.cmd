0a3ea7924b4d87385b7e2f7bac636ca9e122aaaf
Allister Beharry <allister.beharry@gmail.com>
2017-03-13 08:06:48 -0400

    Save original dir and change to build dir.

--- build-release.cmd
+++ build-release.cmd
@@ -1,53 +1,82 @@
 @echo off
 @setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
-call "D:\Program Files\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
-set ERRORCODE=0
+set ERROR_CODE=0
+set DIR=%CD%
 if "%FrameworkDir%" == "" goto EnvError
-if "%1" == "" goto ArgError
-if "%2" == "" goto ArgError
-if "%3" == "" goto ArgError
-if "%4" == "" goto ArgError
-if "%5" == "" goto ArgError
-if not exist "DevAudit.sln" goto SlnError
-if not exist "%5" goto OutputDirError
+where /q git
+if %ERRORLEVEL%==1 goto GitError
+if "%1"=="" goto ArgError
+if "%2"=="" goto ArgError
+if "%3"=="" goto ArgError
+if "%4"=="" goto ArgError
 set MAJOR=%1
 set MINOR=%2
 set PATCH=%3
 set BUILD=%4
-set OUTPUT=%5
-set RELEASE_DIR=%OUTPUT%\%MAJOR%.%MINOR%.%PATCH%.%BUILD%\DevAudit
-rmdir /s /q "%RELEASE_DIR%"
-msbuild DevAudit.sln /p:Configuration=RuntimeDebug;OutputPath=%RELEASE_DIR%;VersionAssembly=%MAJOR%.%MINOR%.%PATCH%.%BUILD%
-
+set SPECIAL=-%5
+if not exist "DevAudit.sln" goto SlnError
+set TAG=v%MAJOR%.%MINOR%.x
+set BUILD_TAG=%MAJOR%.%MINOR%.%PATCH%.%BUILD%
+set RELEASE_TAG=%BUILD_TAG%%SPECIAL%
+set BUILD_DIR=DevAudit.%TAG%.Build_%BUILD%
+if exist %BUILD_DIR% (
+    rmdir /s /q "%BUILD_DIR%"
+)
+if exist %RELEASE_DIR% (
+    rmdir /s /q "%RELEASE_DIR%"
+)
+git clone --branch "%TAG%" https://github.com/OSSIndex/DevAudit %BUILD_DIR%
+if not %ERRORLEVEL% == 0 goto CheckoutError
+mkdir %RELEASE_TAG%
+mkdir %RELEASE_TAG%\DevAudit
+set RELEASE_DIR=%RELEASE_TAG%\DevAudit
+cd %BUILD_DIR%
+build.cmd /VersionAssembly=%MAJOR%.%MINOR%.%PATCH%.%BUILD%
+cd %DIR%
+if not %ERRORLEVEL%==0 goto BuildError
 if not exist ".\Analyzers" goto AnalyzersDirError
 if not exist ".\Rules" goto RulesDirError
 if not exist ".\Examples" goto ExamplesDirError
-mkdir %RELEASE_DIR%\Analyzers & xcopy /E /Y .\Analyzers %RELEASE_DIR%\Analyzers
-mkdir %RELEASE_DIR%\Rules & xcopy /E /Y .\Rules %RELEASE_DIR%\Rules
-mkdir %RELEASE_DIR%\Examples & xcopy /E /Y .\Examples %RELEASE_DIR%\Examples
+xcopy /E /Y %BUILD_DIR%\DevAudit.AuditLibrary\bin\Debug\Gendarme.Rules.*  %BUILD_DIR%\DevAudit.CommandLine\bin\Debug\
+xcopy /E /Y %BUILD_DIR%\DevAudit.CommandLine\bin\Debug\* %RELEASE_DIR%
+mkdir %RELEASE_DIR%\Examples & xcopy /E /Y %BUILD_DIR%\Examples %RELEASE_DIR%\Examples
 copy .\README.md %RELEASE_DIR%
 copy .\LICENSE %RELEASE_DIR%
 goto End
 
 :EnvError
-echo You must run this batch file from a Visual Studio command prompt.
+echo You must run this batch file from a Visual Studio command prompt or run vsvars32.bat before.
+set ERROR_CODE=1
+goto Error
+
+:GitError
+echo Git must be in the current PATH.
+set ERROR_CODE=2
+goto Error
+
+:CheckoutError
+echo There was an error during checkout of branch %TAG%.
+set ERROR_CODE=3
 goto Error
 
 :ArgError
-echo Command syntax is build-release.cmd major minor patch build output_dir.
+echo Command syntax is build-release.cmd major minor patch build [special].
+set ERROR_CODE=4
 goto Error
 
 :SlnError
 echo DevAudit.sln file could not be found.
+set ERROR_CODE=5
 goto Error
 
-:OutputDirError
-echo The output directory %5% could not be found.
+:BuildError
+echo An error occurred during build.
+set ERROR_CODE=6
 goto Error
 
 :AnalyzersDirError
 echo The .\Analyzers directory could not be found. An error may have occurred during build.
+set ERROR_CODE=7
 goto Error
 
 :RulesDirError
@@ -56,11 +85,18 @@ goto Error
 
 :ExamplesDirError
 echo The .\Examples directory could not be found. An error may have occurred during build.
+set ERROR_CODE=7
 goto Error
 
 :Error
-set ERRORCODE=1
+if exist %RELEASE_DIR% (
+    rmdir /s /q "%RELEASE_DIR%"
+)
 
 :End
+if exist %BUILD_DIR% (
+    rmdir /s /q "%BUILD_DIR%"
+)
+cd %DIR%
 @endlocal
-exit /B %ERRORCODE%
+exit /B %ERROR_CODE%
