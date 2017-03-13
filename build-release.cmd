826f69ab196b457075d78b1e0eea0cbdf8a0531a
Allister Beharry <allister.beharry@gmail.com>
2017-03-13 19:46:35 -0400

    Added nuget exe.

--- build-release.cmd
+++ build-release.cmd
@@ -1,4 +1,4 @@
-@echo off
+
 @setlocal
 set ERROR_CODE=0
 set DIR=%CD%
@@ -13,25 +13,30 @@ set MAJOR=%1
 set MINOR=%2
 set PATCH=%3
 set BUILD=%4
-set SPECIAL=-%5
+if not "%5"=="" (
+   set SPECIAL=-%5 
+)
 if not exist "DevAudit.sln" goto SlnError
 set TAG=v%MAJOR%.%MINOR%.x
 set BUILD_TAG=%MAJOR%.%MINOR%.%PATCH%.%BUILD%
 set RELEASE_TAG=%BUILD_TAG%%SPECIAL%
 set BUILD_DIR=DevAudit.%TAG%.Build_%BUILD%
-if exist %BUILD_DIR% (
+set RELEASE_DIR=%RELEASE_TAG%\DevAudit
+if exist "%BUILD_DIR%" (
     rmdir /s /q "%BUILD_DIR%"
 )
-if exist %RELEASE_DIR% (
+if exist "%RELEASE_TAG%" (
+    rmdir /s /q "%RELEASE_TAG%"
+)
+if exist "%RELEASE_DIR%" (
     rmdir /s /q "%RELEASE_DIR%"
 )
 git clone --branch "%TAG%" https://github.com/OSSIndex/DevAudit %BUILD_DIR%
 if not %ERRORLEVEL% == 0 goto CheckoutError
 mkdir %RELEASE_TAG%
 mkdir %RELEASE_TAG%\DevAudit
-set RELEASE_DIR=%RELEASE_TAG%\DevAudit
 cd %BUILD_DIR%
-build.cmd /VersionAssembly=%MAJOR%.%MINOR%.%PATCH%.%BUILD%
+build.cmd "VersionAssembly=%MAJOR%.%MINOR%.%PATCH%.%BUILD%"
 cd %DIR%
 if not %ERRORLEVEL%==0 goto BuildError
 if not exist ".\Analyzers" goto AnalyzersDirError
@@ -56,7 +61,7 @@ goto Error
 
 :CheckoutError
 echo There was an error during checkout of branch %TAG%.
-set ERROR_CODE=3
+set ERROR_CODE=%ERRORLEVEL%
 goto Error
 
 :ArgError
@@ -89,14 +94,14 @@ set ERROR_CODE=7
 goto Error
 
 :Error
-if exist %RELEASE_DIR% (
+if exist "%RELEASE_DIR%" (
     rmdir /s /q "%RELEASE_DIR%"
 )
 
 :End
-if exist %BUILD_DIR% (
+if exist "%BUILD_DIR%" (
     rmdir /s /q "%BUILD_DIR%"
 )
 cd %DIR%
 @endlocal
-exit /B %ERROR_CODE%
+exit /b %ERROR_CODE%
--- build.cmd
+++ build.cmd
@@ -3,16 +3,17 @@
 set ERROR_CODE=0
 .\.nuget\nuget restore DevAudit.sln
 if not %ERRORLEVEL%==0  (
+    echo Error restoring NuGet packages for DevAudit.sln.
     set ERROR_CODE=1
     goto End
 )
 if [%1]==[] (
     msbuild DevAudit.sln /p:Configuration=RuntimeDebug
-)
-else (
+) else (
     msbuild DevAudit.sln /p:Configuration=RuntimeDebug;%*
 )
 if not %ERRORLEVEL%==0  (
+    echo Error building DevAudit.sln.
     set ERROR_CODE=2
     goto End
 )
