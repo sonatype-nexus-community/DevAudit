851f6b74542bc9397ba02eb23b7f6bda4c2665cb
Allister Beharry <allister.beharry@gmail.com>
2017-03-13 22:36:37 -0400

    Add 7za command-line archiver.

--- build.cmd
+++ build.cmd
@@ -1,18 +1,19 @@
 @echo off
 @setlocal
 set ERROR_CODE=0
-.\nuget\nuget restore DevAudit.sln
+.\.nuget\nuget restore DevAudit.sln
 if not %ERRORLEVEL%==0  (
+    echo Error restoring NuGet packages for DevAudit.sln.
     set ERROR_CODE=1
     goto End
 )
 if [%1]==[] (
     msbuild DevAudit.sln /p:Configuration=RuntimeDebug
-)
-else (
+) else (
     msbuild DevAudit.sln /p:Configuration=RuntimeDebug;%*
 )
 if not %ERRORLEVEL%==0  (
+    echo Error building DevAudit.sln.
     set ERROR_CODE=2
     goto End
 )
