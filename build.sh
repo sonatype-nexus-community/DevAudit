4fe8545e186afc2297f43d24ea51f5b9bb40c30d
Ken Duck <ken@qracorp.com>
2017-03-20 22:09:10 -0400

    Ensure build script fails on error

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-rm -r Analyzers/*
-rm -r Rules/*
-rm -r Examples/*
+
+set -e 
+
+rm -fr Analyzers/*
+rm -fr Rules/*
+rm -fr Examples/*
 nuget restore DevAudit.Mono.sln && xbuild DevAudit.Mono.sln /p:Configuration=RuntimeDebug $*
 cp ./DevAudit.AuditLibrary/bin/Debug/Gendarme.Rules.* ./DevAudit.CommandLine/bin/Debug/
