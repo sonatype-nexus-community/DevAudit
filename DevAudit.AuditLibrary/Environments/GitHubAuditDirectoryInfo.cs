47e11726733d15dd0612b83e0f90da2a5026f7ad
Allister Beharry <allister.beharry@gmail.com>
2017-04-19 14:17:26 -0400

    Updated Mono project, removed Gendarme rules references from AuditLibrary.

--- DevAudit.AuditLibrary/Environments/GitHubAuditDirectoryInfo.cs
+++ DevAudit.AuditLibrary/Environments/GitHubAuditDirectoryInfo.cs
@@ -113,10 +113,10 @@ namespace DevAudit.AuditLibrary
             IReadOnlyList<RepositoryContent> c = this.GitHubAuditEnvironment.GetContent(this.FullName);
             if (c == null)
             {
-                this.AuditEnvironment.Warning("Could not get directories for path {0}.", this.FullName);
+                this.AuditEnvironment.Warning("Could not get files for path {0}.", this.FullName);
                 return null;
             }
-            else if (c.Count == 0 || c.Where(content => content.Type == ContentType.Dir).Count() == 0)
+            else if (c.Count == 0 || c.Where(content => content.Type == ContentType.File).Count() == 0)
             {
                 return null;
             }
@@ -138,7 +138,7 @@ namespace DevAudit.AuditLibrary
                 this.AuditEnvironment.Warning("Could not get directories for path {0}.", this.FullName);
                 return null;
             }
-            else if (c.Count == 0 || c.Where(content => content.Type == ContentType.Dir).Count() == 0)
+            else if (c.Count == 0 || c.Where(content => content.Type == ContentType.File).Count() == 0)
             {
                 return null;
             }
