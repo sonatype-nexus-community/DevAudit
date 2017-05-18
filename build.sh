92ed2541705e6a7ce40def91538bf74dd3e17e6c
Allister Beharry <allister.beharry@gmail.com>
2017-05-18 08:43:36 -0400

    Remove copying Gendarme assemblies from audit lib dir.

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ rm -fr Analyzers/*
 rm -fr Rules/*
 rm -fr Examples/*
 nuget restore DevAudit.Mono.sln && xbuild DevAudit.Mono.sln /p:Configuration=RuntimeDebug $*
-cp ./DevAudit.AuditLibrary/bin/Debug/Gendarme.Rules.* ./DevAudit.CommandLine/bin/Debug/
