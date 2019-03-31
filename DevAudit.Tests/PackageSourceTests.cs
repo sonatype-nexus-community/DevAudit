5d96aa0e6e86cee981818c1d7a98ee8be736100d
Allister Beharry <allister.beharry@gmail.com>
2019-03-31 13:52:30 -0400

    Add GetMinimumPackageVersion and other DPS base tests.

--- DevAudit.Tests/PackageSourceTests.cs
+++ DevAudit.Tests/PackageSourceTests.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 
 using Xunit;
 
+using Sprache;
+using Versatile;
+
 using DevAudit.AuditLibrary;
 
 namespace DevAudit.Tests
@@ -25,8 +28,12 @@ namespace DevAudit.Tests
         protected abstract PackageSource Source { get; }
         #endregion
 
-        #region Abstract methods
+        #region Abstract tests
         public abstract void CanTestVulnerabilityVersionInPackageVersionRange();
+
+        public abstract void IsDeveloperPackageSource();
+
+        public abstract void CanGetDPSMinimumPackageVersion();
         #endregion
 
         #region Properties
@@ -37,7 +44,9 @@ namespace DevAudit.Tests
 
         #region Methods
         protected static void EnvironmentMessageHandler(object sender, EnvironmentEventArgs e) { }
+        #endregion
 
+        #region Tests
         [Fact]
         public virtual void CanConstructPackageSource()
         {
@@ -58,7 +67,58 @@ namespace DevAudit.Tests
         }
 
         [Fact]
-        public abstract void CanGetMinimumPackageVersion();
+        public void SemanticVersionCanParseRange()
+        {
+            var comparatorSets = SemanticVersion.Grammar.Range.Parse("6.1.0");
+            Assert.NotEmpty(comparatorSets);
+            Assert.Single(comparatorSets);
+            var comparatorSet = comparatorSets.Single();
+            var a = comparatorSet[0];
+            Assert.Equal(ExpressionType.Equal, a.Operator);
+
+            comparatorSet = SemanticVersion.Grammar.Range.Parse("<2.0.0").Single();
+            Assert.Equal(2, comparatorSet.Count);
+            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.LessThan);
+
+            var max = comparatorSet.Single(c => c.Operator == ExpressionType.LessThan);
+            Assert.Equal(2, max.Version.Major);
+            var min = comparatorSet.Single(c => c.Operator == ExpressionType.GreaterThan);
+            Assert.Equal(0, min.Version.Major);
+
+            comparatorSet = SemanticVersion.Grammar.Range.Parse("<=3.1.4").Single();
+            Assert.Equal(2, comparatorSet.Count);
+            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.LessThanOrEqual);
+            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.GreaterThan);
+
+            max = comparatorSet.Single(c => c.Operator == ExpressionType.LessThanOrEqual);
+            Assert.Equal(3, max.Version.Major);
+            min = comparatorSet.Single(c => c.Operator == ExpressionType.GreaterThan);
+            Assert.Equal(0, min.Version.Major);
+
+            comparatorSet = SemanticVersion.Grammar.Range.Parse(">0.4.2").Single();
+            Assert.Equal(2, comparatorSet.Count);
+            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.GreaterThan);
+            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.LessThan);
+
+            max = comparatorSet.Single(c => c.Operator == ExpressionType.LessThan);
+            Assert.Equal(1000000, max.Version.Major);
+            min = comparatorSet.Single(c => c.Operator == ExpressionType.GreaterThan);
+            Assert.Equal(4, min.Version.Minor);
+
+            comparatorSet = SemanticVersion.Grammar.Range.Parse(">=2.7.1").Single();
+            Assert.Equal(2, comparatorSet.Count);
+            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.GreaterThanOrEqual);
+            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.LessThan);
+
+            max = comparatorSet.Single(c => c.Operator == ExpressionType.LessThan);
+            Assert.Equal(1000000, max.Version.Major);
+            min = comparatorSet.Single(c => c.Operator == ExpressionType.GreaterThanOrEqual);
+            Assert.Equal(1, min.Version.Patch);
+
+            comparatorSet = SemanticVersion.Grammar.Range.Parse("=4.6.6").Single();
+            Assert.Single(comparatorSet);
+            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.Equal);
+        }
         #endregion
     }
 }
--- DevAudit.Tests/YarnPackageSourceTests.cs
+++ DevAudit.Tests/YarnPackageSourceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -6,8 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xunit;
-using Sprache;
-using Versatile;
+
 
 using DevAudit.AuditLibrary;
 
@@ -15,6 +15,7 @@ namespace DevAudit.Tests
 {
     public class Yarn : PackageSourceTests 
     {
+        #region Overriden properties
         protected override PackageSource Source { get; } = 
             new YarnPackageSource(new Dictionary<string, object>()
             { 
@@ -22,7 +23,9 @@ namespace DevAudit.Tests
                 {"LockFile", @".\Examples\yarn.lock.2"}
             
             }, EnvironmentMessageHandler);
-        
+        #endregion
+
+        #region Overriden tests
         [Fact]
         public override void CanTestVulnerabilityVersionInPackageVersionRange()
         {
@@ -33,71 +36,47 @@ namespace DevAudit.Tests
         }
 
         [Fact]
-        public void YarnIsDeveloperPackageSource()
+        public override void IsDeveloperPackageSource()
         {
             Assert.NotNull(DPS);
         }
 
         [Fact]
-        public void CanTestPackageVersionIsRange()
+        public override void CanGetDPSMinimumPackageVersion()
+        {
+            var v = DPS.GetMinimumPackageVersions(">=2.7.1");
+            Assert.Single(v);
+            Assert.EndsWith("1", v.Single());
+
+            v = DPS.GetMinimumPackageVersions(">2.7.1");
+            Assert.Single(v);
+
+            v = DPS.GetMinimumPackageVersions("<2.6.1");
+            Assert.Single(v);
+            Assert.StartsWith("0", v.Single());
+
+            v = DPS.GetMinimumPackageVersions("<=2.7.1");
+            Assert.Single(v);
+            Assert.EndsWith("1", v.Single());
+
+            
+            v = DPS.GetMinimumPackageVersions("^2.7.1");
+            Assert.Single(v);
+            Assert.EndsWith("1", v.Single());
+
+            v = DPS.GetMinimumPackageVersions("~2.7.1");
+            Assert.Single(v);
+            Assert.EndsWith("1", v.Single());
+        }
+
+        #endregion
+
+        [Fact]
+        public void CanTestDPSPackageVersionIsRange()
         {
             Assert.False(DPS.PackageVersionIsRange("6.4.2"));
             Assert.True(DPS.PackageVersionIsRange("~5.1"));
             Assert.False(DPS.PackageVersionIsRange("=7.3"));
-        }
-        
-        [Fact]
-        public override void CanGetMinimumPackageVersion()
-        {
-            var comparatorSets = SemanticVersion.Grammar.Range.Parse("6.1.0");
-            Assert.NotEmpty(comparatorSets);
-            Assert.Single(comparatorSets);
-            var comparatorSet = comparatorSets.Single();
-            var a = comparatorSet[0];
-            Assert.Equal(ExpressionType.Equal, a.Operator);
-
-            comparatorSet = SemanticVersion.Grammar.Range.Parse("<2.0.0").Single();
-            Assert.Equal(2, comparatorSet.Count);
-            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.LessThan);
-            
-            var max = comparatorSet.Single(c => c.Operator == ExpressionType.LessThan);
-            Assert.Equal(2, max.Version.Major);
-            var min = comparatorSet.Single(c => c.Operator == ExpressionType.GreaterThan);
-            Assert.Equal(0, min.Version.Major);
-             
-            comparatorSet = SemanticVersion.Grammar.Range.Parse("<=3.1.4").Single();
-            Assert.Equal(2, comparatorSet.Count);
-            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.LessThanOrEqual);
-            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.GreaterThan);
-
-            max = comparatorSet.Single(c => c.Operator == ExpressionType.LessThanOrEqual);
-            Assert.Equal(3, max.Version.Major);
-            min = comparatorSet.Single(c => c.Operator == ExpressionType.GreaterThan);
-            Assert.Equal(0, min.Version.Major);
-             
-            comparatorSet = SemanticVersion.Grammar.Range.Parse(">0.4.2").Single();
-            Assert.Equal(2, comparatorSet.Count);
-            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.GreaterThan);
-            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.LessThan);
-
-            max = comparatorSet.Single(c => c.Operator == ExpressionType.LessThan);
-            Assert.Equal(1000000, max.Version.Major);
-            min = comparatorSet.Single(c => c.Operator == ExpressionType.GreaterThan);
-            Assert.Equal(4, min.Version.Minor);
-
-            comparatorSet = SemanticVersion.Grammar.Range.Parse(">=2.7.1").Single();
-            Assert.Equal(2, comparatorSet.Count);
-            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.GreaterThanOrEqual);
-            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.LessThan);
-
-            max = comparatorSet.Single(c => c.Operator == ExpressionType.LessThan);
-            Assert.Equal(1000000, max.Version.Major);
-            min = comparatorSet.Single(c => c.Operator == ExpressionType.GreaterThanOrEqual);
-            Assert.Equal(1, min.Version.Patch);
-
-            comparatorSet = SemanticVersion.Grammar.Range.Parse("=4.6.6").Single();
-            Assert.Single(comparatorSet);
-            Assert.Contains(comparatorSet, c => c.Operator == ExpressionType.Equal);
         }
     }
 }
