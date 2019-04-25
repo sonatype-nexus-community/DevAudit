4598afb75c37a813d21dc3540b00d3b3e975bae8
DamianBis <daimob@gmail.com>
2019-04-25 23:28:58 +1000

    update namespace

--- DevAudit.AuditLibrary/Serializers/PackageSourceSerializer.cs
+++ DevAudit.AuditLibrary/Serializers/PackageSourceSerializer.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DevAudit.AuditLibrary.Serializers
+namespace DevAudit.AuditLibrary
 {
     public class PackageSourceSerializer : JsonConverter
     {
