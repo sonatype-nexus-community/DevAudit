08ae17b567db2b2c1aa7a9778ec82eab98ac706d
DamianBis <daimob@gmail.com>
2019-04-25 23:26:06 +1000

    Move Package Source Serializer to its own file

--- DevAudit.AuditLibrary/Targets/PackageSource.cs
+++ DevAudit.AuditLibrary/Targets/PackageSource.cs
@@ -15,42 +15,6 @@ using Newtonsoft.Json;
 
 namespace DevAudit.AuditLibrary
 {
-    public class PackageSourceSerializer : JsonConverter
-    {
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            PackageSource val = (PackageSource)value;
-
-            writer.WriteStartObject();
-            writer.WritePropertyName("Packages Audited");
-            serializer.Serialize(writer, val.Vulnerabilities.Values.Count());
-            writer.WritePropertyName("Vulnerabilities Found");
-            int total_vulnerabilities = val.Vulnerabilities.Sum(v => v.Value != null ? v.Value.Count(pv => pv.PackageVersionIsInRange) : 0);
-            serializer.Serialize(writer, total_vulnerabilities);
-            writer.WritePropertyName("Packages");
-            writer.WriteStartArray();
-            foreach(var vul in val.Vulnerabilities)
-            {
-                writer.WriteStartObject();
-                writer.WritePropertyName("Package");
-                serializer.Serialize(writer, vul.Key);
-                writer.WritePropertyName("Vulnerabilities");
-                serializer.Serialize(writer, vul.Value);
-                writer.WriteEndObject();
-            }
-            writer.WriteEndObject();
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            return typeof(PackageSource).IsAssignableFrom(objectType);
-        }
-    }
 
     [JsonConverter(typeof(PackageSourceSerializer))]
     public abstract class PackageSource : AuditTarget
